e1e55862b119ade1a1d456b09d3fb2002871c612
rexs <admilatoriq@gmail.com>
2025-12-30 07:25:25 +0700

    first commit

---
+++ app/script/index.js
@@ -0,0 +1,25 @@
+const body = document.body;
+const wadahSurah = document.getElementById('list-surah');
+
+const list = document.createElement('ul');
+wadahSurah.append(list);
+
+fetch('https://equran.id/api/v2/surat')
+    .then(res => res.json())
+    .then(data => {
+            const isiData = data.data;
+            for (let i in isiData) {
+                const li = document.createElement('li');
+                const linkSurah = document.createElement('a');
+                linkSurah.setAttribute('href', `surah.html?surah=${isiData[i].nomor}`);
+                li.setAttribute('class', 'list-surah');
+                linkSurah.setAttribute('class', 'nama-surah');
+                linkSurah.innerText = isiData[i].nama;
+                li.append(linkSurah);
+                list.append(li);
+            }
+        }
+    )
+    .catch(err => console.error(err));
+    
+// pembuatan query parameter
---
+++ app/script/surah.js
@@ -0,0 +1,35 @@
+const wadahAyat = document.getElementById('ayat-surah');
+
+const ul = document.createElement('ul');
+wadahAyat.append(ul);
+
+const params = new URLSearchParams(window.location.search);
+const nomorSurah = params.get('surah');
+
+
+fetch(`https://equran.id/api/v2/surat/${nomorSurah}`)
+    .then(res => res.json())
+    .then(data => {
+        const ayat = data.data.ayat;
+        for (const i in ayat) {
+            const li = document.createElement('li');
+            const p = document.createElement('p');
+            p.setAttribute('class', 'text');
+            p.innerHTML = `${ayat[i].nomorAyat}. ${ayat[i].teksArab} `;
+            p.style.fontSize = '35px'
+            p.style.display = 'flex'
+            p.style.justifyContent = 'right'
+            p.style.textAlign = 'right'
+            li.style.listStyleType = 'none'
+            wadahAyat.style.paddingRight = '20px'
+            
+            li.append(p)
+            
+
+            ul.append(li);
+        }
+        const namaSurah = data.data.namaLatin;
+        document.title = `NGAJI ONLINE | ${namaSurah}`;
+    })
+    .catch(err => console.error(err));
+    
