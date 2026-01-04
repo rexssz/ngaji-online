2b1701deac581267e95a9e32913530b80908b7c0
unknown <admilatoriq@gmail.com>
2026-01-04 17:08:33 +0700

    update tampilan navbar dan optimisasi tampilan surah

--- app/script/nav.js
+++ app/script/nav.js
@@ -1,13 +1,17 @@
 const nav = document.querySelector('nav')
 const div = document.createElement('div')
+const a = document.createElement('a')
 const span = document.createElement('span')
 
 div.setAttribute('class', 'container-fluid')
-span.setAttribute('class', 'navbar-brand pt-xxl-3 fs-5 h1')
+span.setAttribute('class', 'navbar-brand fs-5 h1')
+a.setAttribute('class', 'text-decoration-none')
+a.setAttribute('href', 'index.html')
 span.textContent = 'SOROGAN KUY'
 span.style.fontFamily = 'Inter'
 span.style.color = '#fff'
-div.append(span)
+a.append(span)
+div.append(a)
 nav.append(div)
 
 
